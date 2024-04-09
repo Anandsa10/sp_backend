5599f758145793a0a77b24354674583927325efc
Anandsa10 <anandsajeev10@gmail.com>
2024-04-09 13:36:08 +0530

    backend

---
+++ src/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+function initDB() {
+  db.$connect().then(() => {
+    console.log("Connected to database");
+  });
+}
+
+export { db, initDB };
+
+
---
+++ src/group/services.ts
@@ -0,0 +1,17 @@
+import { Group } from '@prisma/client';
+// import { Group } from "@prisma/client";
+import { db } from "../db";
+
+async function getGroupById(id: string) {
+  return await db.group.findFirst({
+    where: {
+      id: id,
+    },
+  });
+}
+
+function userInGroup(group: Group, uid: string) {
+  return group.usersIds.includes(uid);
+}
+
+export { getGroupById, userInGroup };
