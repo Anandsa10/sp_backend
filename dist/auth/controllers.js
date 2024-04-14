f6726556295dd918442d201adc236348755644c7
Anandsa10 <77564172+Anandsa10@users.noreply.github.com>
2024-04-14 15:15:22 +0530

    Update controllers.js

--- dist/auth/controllers.js
+++ dist/auth/controllers.js
@@ -26,6 +26,7 @@ function signUpController(req, res, next) {
         if (password.length < 8 || !/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/.test(password)) {
             next(new types_1.BadRequestError("Password must be at least 8 characters long and contain at least one special character"));
             return;
+        }
         // Check if the username is taken
         const existingUser = yield db_1.db.user.findFirst({
             where: {
